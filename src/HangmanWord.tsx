671b55e37e3ef19908a81c87887fb7dd15b62bb4
Dovran <dovranrustemov@gmail.com>
2022-11-04 10:54:06 -0400

    Finishing Keyboard

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,25 +1,35 @@
-export function HangmanWord() {
-  const word = 'test'
-  const guessedLetters = ['t', 'e', 'g']
+type HangmanWordProps = {
+  guessedLetters: string[]
+  wordToGuess: string
+  reveal?: boolean
+}
 
+export function HangmanWord({
+  guessedLetters,
+  wordToGuess,
+  reveal = false,
+}: HangmanWordProps) {
   return (
     <div
       style={{
         display: 'flex',
         gap: '.25em',
-        fontSize: '2rem',
+        fontSize: '6rem',
         fontWeight: 'bold',
         textTransform: 'uppercase',
         fontFamily: 'monospace',
       }}
     >
-      {word.split('').map((letter, index) => (
-        <span style={{ borderBottom: '.1em soliid black' }} key={index}>
+      {wordToGuess.split('').map((letter, index) => (
+        <span style={{ borderBottom: '.1em solid black' }} key={index}>
           <span
             style={{
-              visibility: guessedLetters.includes(letter)
-                ? 'visible'
-                : 'hidden',
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? 'visible'
+                  : 'hidden',
+              color:
+                !guessedLetters.includes(letter) && reveal ? 'red' : 'black',
             }}
           >
             {letter}
